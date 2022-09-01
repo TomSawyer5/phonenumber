37e6c12785ce6d5067c29728a4ca5b3f5a1455c1
sawyerpascoe <sawyerpascoe@github.com>
2022-08-31 18:40:56 -0700

    simple first script

--- number.py
+++ number.py
@@ -1,1 +1,9 @@
+import phonenumbers
 
+from phonenumbers import geocoder
+
+a = input("Enter the Phone Number: ")
+
+phonenumber = phonenumbers.parse(a)
+
+print(geocoder.description_for_number(phonenumber,'en'))
